df573fa401af071128b84aecdfa9f5da40834c65
davidcopley <davidnicholascopley@hotmail.com>
2017-04-17 19:27:07 +1000

    fixed stupid bug

--- client/controllers/books.js
+++ client/controllers/books.js
@@ -7,7 +7,7 @@ var myApp = angular.module('myApp');
 myApp.controller('BooksController',['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
     console.log('BooksController loaded...');
     $scope.getBooks = function() {
-        $http.get('/api/books').success(function (response) {
+        $http.get('/api/books').then(function (response) {
             $scope.books = response;
         });
     }
